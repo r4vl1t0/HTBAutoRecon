0304bdeb03d4183f04036ea7ea49d976b98041c3
r4vl1t0 <raulitoscontacto@gmail.com>
2025-07-28 22:34:15 -0500

    Update HTBAutoRecon.sh

--- HTBAutoRecon.sh
+++ HTBAutoRecon.sh
@@ -3,6 +3,9 @@
 echo "[+] Made By: Raulitos"
 echo "[+] Github https://github.com/r4vl1t0/HTBAutoRecon"
 
+sudo su -c exit
+
+
 # Verificar que se proporcione una IP
 if [ $# -eq 0 ]; then
     echo "[!] Uso: $0 <IP_TARGET>"
@@ -23,19 +26,24 @@ TmuxInvoke_Windows() {
         tmux split-window -h -t $SESSION_NAME
         tmux split-window -v -t $SESSION_NAME
     
-        tmux send-keys -t $SESSION_NAME:0.1 "rpcclient -U '$User_Windows'%'$Pass_Windows' $TARGET_IP" Enter
+        #rpcclient
+        tmux send-keys -t $SESSION_NAME:0.1 "rpcclient -U '$User_Windows'%'$Pass_Windows' $TARGET_IP -c enumdomusers | cut -d "[" -f 2 | cut -d "]" -f 1" Enter
         
         # Cambiar al panel izquierdo para el escaneo    
         tmux select-pane -t $SESSION_NAME:0.0
         
-        # Script para ejecutar dentro de tmux
+        # nmap scan
         tmux send-keys -t $SESSION_NAME:0.0 "tail -f scan" Enter
         
+        # nxc smb shares
         tmux send-keys -t $SESSION_NAME:0.2 "nxc smb $TARGET_IP -u '$User_Windows' -p '$Pass_Windows' --shares" Enter
+
+        # bloodhound
         tmux send-keys -t $SESSION_NAME:0.3 "bloodhound-python -u '$User_Windows' -p '$Pass_Windows' -d $TARGET_DOM -c all --zip -ns $TARGET_IP" Enter
 
 }
 
+# agregar si no hay puerto 80 - buscar otro puerto  - grepear el nmap para hacerle whhatweb
 TmuxInvoke_Linux() {
 
         # Crear nueva sesión tmux
@@ -49,16 +57,16 @@ TmuxInvoke_Linux() {
         tmux select-pane -t $SESSION_NAME:0.1
         tmux split-window -v -t $SESSION_NAME
 
-        tmux send-keys -t $SESSION_NAME:0.2 "ffuf -c -w /usr/share/SecLists/Discovery/DNS/bitquark-subdomains-top100000.txt -u http://$TARGET_DOM -H 'Host: FUZZ.$TARGET_DOM' -ac" Enter
+        tmux send-keys -t $SESSION_NAME:0.2 "ffuf -c -w /usr/share/SecLists/Discovery/DNS/bitquark-subdomains-top100000.txt -u 'http://$TARGET_DOM/' -H 'Host: FUZZ.$TARGET_DOM' -ac" Enter
                 
         # Cambiar al panel izquierdo para el escaneo    
         tmux select-pane -t $SESSION_NAME:0.0
         
         # Script para ejecutar dentro de tmux
         tmux send-keys -t $SESSION_NAME:0.0 "tail -f scan" Enter
-        tmux send-keys -t $SESSION_NAME:0.1 "dirsearch -u http://$TARGET_DOM/ -F -f -x 404" Enter  
+        tmux send-keys -t $SESSION_NAME:0.1 "dirsearch -u http://$TARGET_DOM/ -F -f -x 404" Enter
         tmux send-keys -t $SESSION_NAME:0.3 "curl -k -i $TARGET_IP" Enter     
-        tmux send-keys -t $SESSION_NAME:0.4 "whatweb -v $TARGET_IP" Enter
+        tmux send-keys -t $SESSION_NAME:0.4 "whatweb $TARGET_IP" Enter
         
 
 }
@@ -67,6 +75,7 @@ echo ""
 echo "[+] Bienvenido al programa de automatización para HTB"
 echo "[+] Iniciando reconocimiento de sistema operativo: $TARGET_IP..."
 
+# !!!!! hacerlo repetitivo por errores
 PingSo () {
     PING_RESULT=$(ping -c 1 $TARGET_IP 2>/dev/null)
 
@@ -88,7 +97,7 @@ PingSo () {
 }
 
 TakingDomain_Linux(){
-    TARGET_DOM=$(curl -i -k $TARGET_IP --silent | grep "Location" | cut -d "/" -f 3)
+    TARGET_DOM=$(curl -i -k $TARGET_IP --silent | grep "Location" | cut -d "/" -f 3 | tr -d '\r\n')
     echo "[+] El dominio es: $TARGET_DOM"
     CHECKER_DOM=$(echo "$TARGET_IP")
 
@@ -110,18 +119,26 @@ TakingDomain_Windows(){
     if grep -q "$CHECKER_DOM" /etc/hosts; then
         echo "[*] Ya está presente en /etc/hosts"
     else
-        echo "$CHECKER_DOM  $TARGET_DOM" | sudo tee -a /etc/hosts > /dev/null
+        echo "$CHECKER_DOM  $TARGET_DOM" |  sudo tee -a /etc/hosts > /dev/null
         echo "[+] Agregado a /etc/hosts"
     fi   
 }
 
+# --script vuln
 NmapFunction(){
-    nmap -sS -sCV -p- --min-rate 5000 -Pn -n --disable-arp-ping --open $TARGET_IP -oN scan -v 1>/dev/null &
+     sudo nmap -sS -sCV -p- --min-rate 5000 -Pn -n --disable-arp-ping --open $TARGET_IP -oN $(pwd)/scan -v 1>/dev/null &
 }
 
+Grep_Ports(){
+    PORTS1=$(sudo nmap -F -sS --min-rate 5000 -n -Pn --open $TARGET_IP | grep "tcp" | cut -d "/" -f 1 | grep -Ev "Not|22" > /tmp/ports.log)
+}
+
+Grep_Ports
+PORTS=/tmp/ports.log
 NmapFunction
 PingSo
-echo "[+] El sistema operativo es: $OSType"
+ 
+echo -e  "\t[+] El sistema operativo es: $OSType"
 
 if [ "$OSType" == "LINUX" ]; then
     TakingDomain_Linux
@@ -142,11 +159,12 @@ elif [ "$OSType" == "WINDOWS" ];then
 
     elif [ "$Respuesta" == "n" ]; then
         echo "[+] Continuando con escaneo"
+        TakingDomain_Linux
         TmuxInvoke_Linux
 
     else
-        echo "[+] No seas payaso... Ejecutando malware..."
-        sleep 5
+        echo "[!] Input Incorrecto... "
+        sleep 2
         exit 1
     fi
 
@@ -155,6 +173,3 @@ fi
 sleep 3
 tmux attach-session -t $SESSION_NAME
 
-# AÑADIR RECONOCMIENTO DE OS SIN NECESIDAD DEL TTL
-# QUITAR EL DISABLE-ARP-PING
-# ADELANTAR TAREA CON EL NMAP EN VES DE LANZARLO EN EL TMUX
